9c1cd7e90dfdae4c4447bf6fcb796783921b0a3c
Pdp4/8wzNRDhYscPJ4RYjjc5PYPR59KNV2b76KSOzBU= <IAMUserInfyTraining1+1-at-038792456041>
2021-03-16 16:08:32 +0530

    second

--- src/test/java/com/infosys/OrderMsApplicationTests.java
+++ src/test/java/com/infosys/OrderMsApplicationTests.java
@@ -1,17 +1,45 @@
 package com.infosys;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+import com.infosys.dto.ProductsOrderedDTO;
+import com.infosys.repository.ProductsOrderedRepo;
+import com.infosys.service.OrderService;
+
+@RunWith(MockitoJUnitRunner.class)
 @SpringBootTest
-class OrderMsApplicationTests {
+public class OrderMsApplicationTests {
+	 @Mock
+	    public ProductsOrderedRepo ordrepo;
+	    
+	    @InjectMocks
+	    public OrderService orderService=new OrderService();
+	    
+	    
+	    
+	    @Test
+	    public void getOrderDetailsBySellerIdTest() {
+	        List<ProductsOrderedDTO> productsOrderedDTOs=orderService.getOrderDetailsBySellerId(1);
+	        Assertions.assertEquals(productsOrderedDTOs.size(), 0);
+	    }
+	    
+	    @Test
+	    public void getOrderDetailsBySellerIdTestInvalid() {
+	        List<ProductsOrderedDTO> productsOrderedDTOList=new ArrayList<>();
+	        Mockito.lenient().when(orderService.getOrderDetailsBySellerId(16)).thenReturn(productsOrderedDTOList);
+	        List<ProductsOrderedDTO> productsOrderedDTOs=orderService.getOrderDetailsBySellerId(16);
+	        Assertions.assertEquals(productsOrderedDTOs.size(), 0);
+	    }
 
-	@Test
-	
-	void contextLoads() {
-	}
 
 }
